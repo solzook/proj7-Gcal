589bba8906dc1d73922a2d638b30414d04766d62
Solomon Zook <szook@uoregon.edu>
2016-11-21 12:58:51 -0800

    adding nosetests

--- free_times.py
+++ free_times.py
@@ -42,11 +42,8 @@ def get_free_times(begin_time, end_time, begin_date, end_date, event_list):
     date1 = arrow.get(begin_date)
     date2 = arrow.get(end_date)
 
-    print("date range is {}-{}".format(date1,date2))
-
     free_times = []
     for day in arrow.Arrow.span_range('day', date1, date2):
-        print("\tlooking in day {}-{}".format(day[0],day[1]))
         apt_today = Agenda.Appt(day[0].date(), time1, time2, "Free time on {}".format(day[0].format("YYYY/MM/DD")))
         free_today = busy_agenda.complement(apt_today)
         free_times.append([])
