bca80adeb2688871cbc2b4c6eaa6c911231c5a95
Solomon Zook <szook@uoregon.edu>
2016-12-09 01:53:56 -0800

    implementing database

--- db_interactions.py
+++ db_interactions.py
@@ -2,8 +2,29 @@
 Author: Solomon Zook
 database interactions for the program are in this file
 """
+
 import pymongo
+from pymongo import MongoClient
+import sys
+
 import secrets.client_secrets
+import secrets.admin_secrets
+
+
+MONGO_ADMIN_URL = "mongodb://{}:{}@{}:{}/admin".format(
+    secrets.admin_secrets.admin_user,
+    secrets.admin_secrets.admin_pw,
+    secrets.admin_secrets.host, 
+    secrets.admin_secrets.port)
+
+try: 
+    dbclient = MongoClient(MONGO_ADMIN_URL)
+    db = getattr(dbclient, secrets.client_secrets.db)
+    print("Got database")
+except Exception as err:
+    print("Failed")
+    print(err)
+
 
 def add_meeting_info(begin_time_range, end_time_range, begin_date_range, end_date_range):
     """
@@ -15,20 +36,18 @@ def add_meeting_info(begin_time_range, end_time_range, begin_date_range, end_dat
         begin_date_range:
         end_date_range:
     """
-    collection = 'db.{}'.format(secrets.client_secrets.db)
     to_add = {}
     to_add['st_time'] = begin_time_range
     to_add['end_time'] = end_time_range
     to_add['st_date'] = begin_date_range
     to_add['end_date'] = end_date_range
     
-    collection.insert(to_add)
+    db.insert(to_add)
 
 def show_db():
     """
     print info from the database
     """
-    collection = 'db.{}'.format(secrets.client_secrets.db)
     
-    collection.find()
+    db.find()
         
